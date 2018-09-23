e82a8bf003caa2b71a28f111cd889deb47075cce
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-09-23 18:45:23 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan11/formatting/PBO310117096Latihan11Formatting.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan11.formatting;
+
+/**
+ *
+ * @author
+ * NAMA         : MIFTAH MUHAMMAD SIDQI
+ * NIM          : 10117096
+ * KELAS        : PBO3
+ * DESKRIPSI    : Program ini berisi program untuk menampilkan data formatting
+ */
+public class PBO310117096Latihan11Formatting {
+
+    
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+    
+}
